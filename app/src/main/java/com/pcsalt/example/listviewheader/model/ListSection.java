aaa5ec3b73665b5f6853ec0f406c6914f788ad5a
krrishnaaaa <krishna@pcsalt.com>
2015-12-09 23:37:56 +0530

    created model for section title

---
+++ app/src/main/java/com/pcsalt/example/listviewheader/model/ListSection.java
@@ -0,0 +1,16 @@
+package com.pcsalt.example.listviewheader.model;
+
+/**
+ * Created by Krrishnaaaa on Dec 08, 2015
+ */
+public class ListSection extends ListData {
+    String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
