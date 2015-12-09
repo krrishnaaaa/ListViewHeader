c4aeac953a58cc542877b8ae8d6e2dff187d7c48
krrishnaaaa <krishna@pcsalt.com>
2015-12-09 23:39:43 +0530

    displaying sectioned data in ListView

--- app/src/main/java/com/pcsalt/example/listviewheader/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/listviewheader/MainActivity.java
@@ -1,13 +1,53 @@
 package com.pcsalt.example.listviewheader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.pcsalt.example.listviewheader.adapter.SectionedBaseAdapter;
+import com.pcsalt.example.listviewheader.model.ListData;
+import com.pcsalt.example.listviewheader.model.ListItem;
+import com.pcsalt.example.listviewheader.model.ListSection;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    List<ListData> dataList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        prepareContent();
+        setAdapter();
     }
+
+    private void initViews() {
+        listView = (ListView) findViewById(R.id.list_view);
+    }
+
+    private void prepareContent() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 0 || i % 7 == 0) {
+                ListSection listSection = new ListSection();
+                listSection.setTitle("Title: " + i);
+                dataList.add(listSection);
+            } else {
+                ListItem listItem = new ListItem();
+                listItem.setMessage(i + " % 7 == 1");
+                listItem.setMessageLine2("" + (i % 7 == 1));
+                dataList.add(listItem);
+            }
+        }
+    }
+
+    private void setAdapter() {
+        SectionedBaseAdapter adapter = new SectionedBaseAdapter(MainActivity.this, dataList);
+        listView.setAdapter(adapter);
+    }
+
 }
